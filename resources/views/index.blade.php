99d0791f5c063d7c4581fb992ee218f3ea796038
Uwayxt <idwahyudi523@gmail.com>
2024-07-04 14:27:58 +0700

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-lg font-bold text-sky-500">test</h1>
+</body>
+</html>
