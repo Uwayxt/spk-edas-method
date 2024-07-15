8ba8264ad17f43209e4956e0254d1f68686c923a
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2024-07-15 21:40:07 +0700

    feat: note

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,17 +1,16 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/css/app.css','resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
   <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 <body>
-
-
-
 @yield('content')
-
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+
 </body>
 </html>
